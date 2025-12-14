969fb146e1c9bd9451e9807da73d61187e46d7a6
João Mateus <mateusgamescoelho547@gmail.com>
2025-12-14 16:38:48 -0300

    fix: add typing

--- landing-page/app/provider.tsx
+++ landing-page/app/provider.tsx
@@ -3,7 +3,7 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
 
-export function PHProvider({ children }) {
+export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
